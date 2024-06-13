7ebb3505f1ab222fc67b96b1be8762662106aae6
Abhishek Pardhiya <abhishekpardhiya@gmail.com>
2024-06-13 20:27:30 +0530

    Project Setup

---
+++ src/main/java/com/pay/RazorpayWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazorpayWithSpringBootApplication.class, args);
+	}
+
+}
